7f1de0aab73fa54b8416c31b7fadbe88bb93aeed
Vansh Chaudhary <vansh28900@gmail.com>
2022-10-02 20:04:40 +0530

    ep1_LEXER

--- shell.py
+++ shell.py
@@ -1,5 +1,12 @@
-import sched
+import os
+import basic
+os.system('cls||clear')
 
 while True:
     text = input("Basic>")
-    print(text)
+    result, error = basic.run('<stdin>',text)
+
+    if error: print(error.as_string(),end='\n\n')
+    else : 
+        print(result,end='\n\n')
+        
